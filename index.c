18b9e27534ee3045eabd3068de22887575c354cd
teddy1565 <teddy1565@gmail.com>
2022-01-18 20:25:31 +0800

    first commit

---
+++ index.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+    int n,m;
+    while(~scanf("%d%d",&n,&m)) {
+        if (n==m) {
+            printf("%d\n",m);
+        } else {
+            printf("%d\n",m+1);
+        }
+    }
+    return 0;
+}
